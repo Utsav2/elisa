485f7633480e698280ad306ce316398b7913c0fd
Huy Le <huyle2@illinois.edu>
2015-08-28 18:42:59 -0500

    Update

---
+++ android/app/src/main/java/uiuc/bioassay/elisa/proc/SampleProcActivity.java
@@ -0,0 +1,371 @@
+package uiuc.bioassay.elisa.proc;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import uiuc.bioassay.elisa.ELISAApplication;
+import uiuc.bioassay.elisa.R;
+
+import static uiuc.bioassay.elisa.ELISAApplication.AVG_FILE_NAME;
+import static uiuc.bioassay.elisa.ELISAApplication.readRGBSpec;
+import static uiuc.bioassay.elisa.ELISAApplication.readSampleResNormalized;
+
+public class SampleProcActivity extends AppCompatActivity {
+    private static final String TAG = "SAMPLE";
+    private int currResult;
+    private String folder;
+    private Bitmap bitmap;
+    private double[] rgb_spec;
+    private double[] bb;
+    private Button currButton = null;
+
+    public void setCurrResult(int result) {
+        currResult = result;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bb_proc);
+
+        folder = getIntent().getStringExtra(ELISAApplication.FOLDER_EXTRA);
+        Log.d(TAG, folder);
+
+        Button done = (Button) findViewById(R.id.done);
+        done.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                }
+        );
+
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final LineChart chart = (LineChart) findViewById(R.id.chart);
+
+        final Button image_button = (Button) findViewById(R.id.image_button);
+        currButton = image_button;
+        image_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (currResult == -1) {
+                            return;
+                        }
+                        currButton.setEnabled(true);
+                        chart.setVisibility(View.INVISIBLE);
+                        if (bitmap == null) {
+                            bitmap = decodeIMG("/storage/emulated/0/Android/data/uiuc.bioassay.elisa/test-elisa/sample" + File.separator + AVG_FILE_NAME);
+                            // TODO: Enable the below in production
+                            //bitmap = decodeIMG(folder + File.separator + AVG_FILE_NAME);
+                        }
+                        imageView.setImageBitmap(bitmap);
+                        imageView.setVisibility(View.VISIBLE);
+                        image_button.setEnabled(false);
+                        currButton = image_button;
+                    }
+                }
+        );
+
+        final Button rgb_button = (Button) findViewById(R.id.rgb_button);
+        rgb_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (currResult == -1) {
+                            return;
+                        }
+                        currButton.setEnabled(true);
+                        imageView.setVisibility(View.INVISIBLE);
+                        if (rgb_spec == null) {
+                            rgb_spec = readRGBSpec("/storage/emulated/0/Android/data/uiuc.bioassay.elisa/test-elisa/sample" + File.separator + ELISAApplication.RGB_SPEC);
+                            // TODO: Enable the below in production
+                            //rgb_spec = readRGBSpec(folder + File.separator + ELISAApplication.RGB_SPEC);
+                        }
+                        setRGBSpecData(chart);
+                        chart.setVisibility(View.VISIBLE);
+                        rgb_button.setEnabled(false);
+                        currButton = rgb_button;
+                    }
+                }
+        );
+
+        final Button normalized_button = (Button) findViewById(R.id.normalized_button);
+        normalized_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (currResult == -1) {
+                            return;
+                        }
+                        currButton.setEnabled(true);
+                        imageView.setVisibility(View.INVISIBLE);
+                        if (bb == null) {
+                            bb = readSampleResNormalized("/storage/emulated/0/Android/data/uiuc.bioassay.elisa/test-elisa/sample" + File.separator + ELISAApplication.RES);
+                            // TODO: Enable the below in production
+                            // bb = readResNormalized(folder + File.separator + ELISAApplication.RES);
+                        }
+                        setBBData(chart);
+                        chart.setVisibility(View.VISIBLE);
+                        normalized_button.setEnabled(false);
+                        currButton = normalized_button;
+                    }
+                }
+        );
+
+        SampleProcWorker sampleProcWorker = new SampleProcWorker(this);
+
+        sampleProcWorker.execute("/storage/emulated/0/Android/data/uiuc.bioassay.elisa/test-elisa/sample");
+        //sampleProcWorker.execute(folder);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tlcproc, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    private static Bitmap decodeIMG(String img) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        Bitmap bitmap = BitmapFactory.decodeFile(img, options);
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    private void setRGBSpecData(LineChart lineChart) {
+        int length = rgb_spec.length / 3;
+
+        double nmScale = (ELISAApplication.RED_LASER_NM - ELISAApplication.GREEN_LASER_NM) /
+                (ELISAApplication.RED_LASER_PEAK - ELISAApplication.GREEN_LASER_PEAK);
+
+        double nmOff = ELISAApplication.GREEN_LASER_NM - nmScale * ELISAApplication.GREEN_LASER_PEAK;
+
+        double[] nm = new double[length];
+        for (int i = 0; i < length; ++i) {
+            nm[i] = nmScale * i + nmOff;
+        }
+
+        int startIdx = 0;
+        while (nm[startIdx] > 700) {
+            ++startIdx;
+        }
+        int endIdx = startIdx;
+        while (endIdx < length && nm[endIdx] >= 380) {
+            ++endIdx;
+        }
+
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int i = endIdx; i >= startIdx; --i) {
+            xVals.add((float) nm[i] + "");
+        }
+
+        double[] red = new double[endIdx - startIdx + 1];
+        double[] green = new double[endIdx - startIdx + 1];
+        double[] blue = new double[endIdx - startIdx + 1];
+        for (int i = startIdx; i <= endIdx; ++i) {
+            red[i-startIdx] = rgb_spec[3*i];
+            green[i-startIdx] = rgb_spec[3*i + 1];
+            blue[i-startIdx] = rgb_spec[3*i + 2];
+        }
+
+
+        ArrayList<Entry> yRedVals = new ArrayList<>();
+        ArrayList<Entry> yGreenVals = new ArrayList<>();
+        ArrayList<Entry> yBlueVals = new ArrayList<>();
+
+        for (int i = endIdx; i >= startIdx; --i) {
+            int idx = endIdx - i;
+            yRedVals.add(new Entry((float)red[i-startIdx], idx));
+            yGreenVals.add(new Entry((float)green[i-startIdx], idx));
+            yBlueVals.add(new Entry((float)blue[i-startIdx], idx));
+        }
+
+
+        LineDataSet setRed = new LineDataSet(yRedVals, "Red Spectrum");
+        setRed.setDrawCircles(false);
+        setRed.setColor(Color.RED);
+        setRed.setLineWidth(1f);
+
+        LineDataSet setGreen = new LineDataSet(yGreenVals, "Green Spectrum");
+        setGreen.setDrawCircles(false);
+        setGreen.setColor(Color.GREEN);
+        setGreen.setLineWidth(1f);
+
+        LineDataSet setBlue = new LineDataSet(yBlueVals, "Blue Spectrum");
+        setBlue.setDrawCircles(false);
+        setBlue.setColor(Color.BLUE);
+        setBlue.setLineWidth(1f);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(setRed); // add the datasets
+        dataSets.add(setGreen);
+        dataSets.add(setBlue);
+
+
+        LineData data = new LineData(xVals, dataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
+        lineChart.setDescription("Wavelength [nm]");
+        lineChart.setDescriptionColor(Color.WHITE);
+
+        // XAxis
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawGridLines(false);
+
+
+        // YAxis
+        YAxis rightAxis = lineChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.setTextSize(10f);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(false);
+
+        // Legend
+        Legend legend = lineChart.getLegend();
+        legend.setTextSize(10f);
+        legend.setTextColor(Color.WHITE);
+
+        lineChart.setTouchEnabled(false);
+        lineChart.setDragEnabled(false);
+        lineChart.setScaleEnabled(false);
+        lineChart.setScaleXEnabled(false);
+        lineChart.setScaleYEnabled(false);
+        lineChart.setPinchZoom(false);
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setHighlightEnabled(false);
+        lineChart.setHighlightPerDragEnabled(false);
+        lineChart.setDrawGridBackground(false);
+    }
+
+    private void setBBData(LineChart lineChart) {
+        int length = bb.length;
+
+        double nmScale = (ELISAApplication.RED_LASER_NM - ELISAApplication.GREEN_LASER_NM) /
+                (ELISAApplication.RED_LASER_PEAK - ELISAApplication.GREEN_LASER_PEAK);
+
+        double nmOff = ELISAApplication.GREEN_LASER_NM - nmScale * ELISAApplication.GREEN_LASER_PEAK;
+
+        double[] nm = new double[length];
+        for (int i = 0; i < length; ++i) {
+            nm[i] = nmScale * i + nmOff;
+        }
+
+        int startIdx = 0;
+        while (nm[startIdx] > 700) {
+            ++startIdx;
+        }
+        int endIdx = startIdx;
+        while (endIdx < length && nm[endIdx] >= 380) {
+            ++endIdx;
+        }
+
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int i = endIdx; i >= startIdx; --i) {
+            xVals.add((float) nm[i] + "");
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<>();
+
+        for (int i = endIdx; i >= startIdx; --i) {
+            int idx = endIdx - i;
+            yVals.add(new Entry((float)bb[i], idx));
+        }
+
+
+        LineDataSet lineDataSet = new LineDataSet(yVals, "Broadband Normalized Spectrum");
+        lineDataSet.setDrawCircles(false);
+        lineDataSet.setColor(Color.CYAN);
+        lineDataSet.setLineWidth(1f);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet); // add the datasets
+
+
+        LineData data = new LineData(xVals, dataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
+        lineChart.setDescription("Wavelength [nm]");
+        lineChart.setDescriptionColor(Color.WHITE);
+
+        // XAxis
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawGridLines(false);
+
+
+        // YAxis
+        YAxis rightAxis = lineChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.setTextSize(10f);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(false);
+
+        // Legend
+        Legend legend = lineChart.getLegend();
+        legend.setTextSize(10f);
+        legend.setTextColor(Color.WHITE);
+
+        lineChart.setTouchEnabled(false);
+        lineChart.setDragEnabled(false);
+        lineChart.setScaleEnabled(false);
+        lineChart.setScaleXEnabled(false);
+        lineChart.setScaleYEnabled(false);
+        lineChart.setPinchZoom(false);
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setHighlightEnabled(false);
+        lineChart.setHighlightPerDragEnabled(false);
+        lineChart.setDrawGridBackground(false);
+    }
+}
---
+++ android/app/src/main/java/uiuc/bioassay/elisa/proc/SampleProcWorker.java
@@ -0,0 +1,80 @@
+package uiuc.bioassay.elisa.proc;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+
+import uiuc.bioassay.elisa.R;
+
+import static uiuc.bioassay.elisa.ELISAApplication.AVG_FILE_NAME;
+import static uiuc.bioassay.elisa.ELISAApplication.processBB;
+import static uiuc.bioassay.elisa.ELISAApplication.processSample;
+
+/**
+ * Created by meowle on 7/8/15.
+ */
+public class SampleProcWorker extends AsyncTask<String, Void, Integer> {
+    private Context mContext;
+    private ProgressDialog progressDialog;
+    private String folder;
+
+    public SampleProcWorker(Context context) {
+        mContext = context;
+        progressDialog = new ProgressDialog(context);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog.setCancelable(false);
+        progressDialog.setTitle("Wait");
+        progressDialog.setMessage("Processing Sample...");
+        progressDialog.show();
+    }
+
+    @Override
+    protected Integer doInBackground(String... params) {
+        folder = params[0];
+        return processSample(folder + File.separator);
+    }
+
+    @Override
+    protected void onPostExecute(Integer result) {
+        progressDialog.dismiss();
+        Activity activity = (Activity) mContext;
+        SampleProcActivity sampleProcActivity = (SampleProcActivity) activity;
+        if (sampleProcActivity == null) {
+            return;
+        }
+        if (result == -1) {
+            TextView textView = (TextView) sampleProcActivity.findViewById(R.id.text_result);
+            textView.setText("Error, unable to process data!");
+            sampleProcActivity.setCurrResult(result);
+            return;
+        }
+        ImageView imageView = (ImageView) sampleProcActivity.findViewById(R.id.imageView);
+        imageView.setImageBitmap(decodeIMG(folder + File.separator + AVG_FILE_NAME));
+
+        Button button = (Button) sampleProcActivity.findViewById(R.id.image_button);
+        button.setEnabled(false);
+    }
+
+    private static Bitmap decodeIMG(String img) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        Bitmap bitmap = BitmapFactory.decodeFile(img, options);
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+}
